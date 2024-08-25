a7498f98c78eacb002ceda0bc6b5940716f401f9
Ian Homer <code@ianhomer.com>
2024-08-25 09:00:57 +0100

    👷 Add test CI

--- foo/test_foo.py
+++ foo/test_foo.py
@@ -1,5 +1,5 @@
 import unittest
-from foo import Foo
+from . import Foo
 
 
 class TestFoo(unittest.TestCase):
@@ -7,6 +7,8 @@ class TestFoo(unittest.TestCase):
     def test_sum(self):
         self.assertEqual(3, Foo(1, 2).sum())
 
+    def test_fail(self):
+        assert False
 
 if __name__ == "__main__":
     unittest.main()
